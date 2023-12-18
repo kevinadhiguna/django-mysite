4058d0e861321e3f444b49e8d0e846aba928aefe
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-18 22:54:17 +0700

    refactor: show details of a question

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("Hello World. You're at polls index.")
+
+def detail(request, question_id):
+  return HttpResponse("You're looking at question %s." % question_id)
