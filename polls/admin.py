91d071f8b32d3fce1431f470333c83cc51b4804a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-20 01:46:50 +0700

    refactor: modify admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
+class QuestionAdmin(admin.ModelAdmin):
+  # Following change makes the “Publication date” comes before the “Question” field
+  fields = ["pub_date", "question_text"]
+
 admin.site.register(Question)
