fd4290593e5cb61a5c75f053973eb50300a2c0c4
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-20 16:22:52 +0700

    feat(admin): enable choice objects to be edited on the question admin page

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
+class ChoiceInline(admin.StackedInline):
+  model = Choice
+  extra = 3       # <- Tells Django to provide enough fields for 3 choices, by default
+
 class QuestionAdmin(admin.ModelAdmin):
   # Following change makes the “Publication date” comes before the “Question” field
   fieldsets = [
     (None, {"fields": ["question_text"]}),
-    ("Date information", {"fields": ["pub_date"]})
+    ("Date information", {"fields": ["pub_date"]}, {"classes": ["collapse"]})
   ]
+  inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
