0c8a099c2ef3b1e355a862150b3829624d4aced9
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-20 16:16:50 +0700

    feat(admin): add fieldsets when changing a question

--- polls/admin.py
+++ polls/admin.py
@@ -5,6 +5,9 @@ from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
   # Following change makes the “Publication date” comes before the “Question” field
-  fields = ["pub_date", "question_text"]
+  fieldsets = [
+    (None, {"fields": ["question_text"]}),
+    ("Date information", {"fields": ["pub_date"]})
+  ]
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
